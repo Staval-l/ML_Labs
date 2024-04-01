a12ca4b60313347c3bb34b78cd092e7f597af70e
Staval <stalkervaultboy@gmail.com>
2024-04-01 22:39:36 +0400

    Исправления во 2 лабе

--- Lab_2/Lab_2.py
+++ Lab_2/Lab_2.py
@@ -2,15 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 
-
 M_0 = np.array([0, 1])
 M_1 = np.array([-1, -1])
 M_2 = np.array([2, 1])
 
-# Task 1 from lab 1
 B_0 = np.array([[0.35, 0.15], [0.1, 0.35]])
 
-# Task 2 from lab 1
 B_1 = np.array([[0.45, 0.15], [0.15, 0.45]])
 B_2 = np.array([[0.15, 0.02], [0.02, 0.15]])
 B_3 = np.array([[0.25, -0.17], [-0.17, 0.25]])
@@ -31,9 +28,9 @@ def boundary_of_bayes_classifier_for_N_with_same_B(x, M_l, M_j, B, threshold):
     # d_lj = ... = ax+b=0
     # a_0*x_0 + a_1*x_1 = const
     a = np.matmul(M_dif_T, B_inv)
-    b = -0.5*np.matmul(np.matmul(M_sum_T, B_inv), M_dif) + threshold  # page 31 случай 2
+    b = -0.5 * np.matmul(np.matmul(M_sum_T, B_inv), M_dif) + threshold  # page 31 случай 2
 
-    return np.array((-b - a[0, 0]*x) / a[0, 1])
+    return np.array((-b - a[0, 0] * x) / a[0, 1])
 
 
 def get_count_fail(x, M_l, M_j, B, threshold):
@@ -44,7 +41,7 @@ def get_count_fail(x, M_l, M_j, B, threshold):
     B_inv = np.linalg.inv(B)
 
     a = np.matmul(M_dif_T, B_inv)
-    b = -0.5 * np.matmul(np.matmul(M_sum_T, B_inv), M_dif) + threshold  # page 31 случай 3
+    b = -0.5 * np.matmul(np.matmul(M_sum_T, B_inv), M_dif) + threshold  # page 31 случай 2
 
     # print(a)
     # print(x)
@@ -57,11 +54,10 @@ def get_count_fail(x, M_l, M_j, B, threshold):
             counter += 1
 
     # print(counter)
-    return counter/x.shape[0]
+    return counter / x.shape[0]
 
 
 def boundary_of_bayes_classifier_for_N(x, M_l, M_j, B_l, B_j, threshold):
-
     M_l_T = M_l.reshape(1, 2)
     M_j_T = M_j.reshape(1, 2)
 
@@ -76,7 +72,7 @@ def boundary_of_bayes_classifier_for_N(x, M_l, M_j, B_l, B_j, threshold):
     A = 0.5 * (B_j_inv - B_l_inv)
     b = np.matmul(M_l_T, B_l_inv) - np.matmul(M_j_T, B_j_inv)
     c = (0.5 * np.matmul(np.matmul(M_j_T, B_j_inv), M_j) - 0.5 * np.matmul(np.matmul(M_l_T, B_l_inv), M_l)
-         + 0.5*np.log(det_B_j/det_B_l) + threshold)  # конец 4 лекции
+         + 0.5 * np.log(det_B_j / det_B_l) + threshold)  # конец 4 лекции
 
     # print(f"A = {A}")
     # print(f"b = {b}")
@@ -86,7 +82,7 @@ def boundary_of_bayes_classifier_for_N(x, M_l, M_j, B_l, B_j, threshold):
     for x0 in x:
         coef_a = A[1, 1]
         coef_b = (A[1, 0] + A[0, 1]) * x0 + b[0, 1]
-        coef_c = A[0, 0] * x0**2 + b[0, 0] * x0 + c[0]
+        coef_c = A[0, 0] * x0 ** 2 + b[0, 0] * x0 + c[0]
         D = coef_b ** 2 - 4 * coef_a * coef_c
         if D > 0:
             x1_0 = (-coef_b + np.sqrt(D)) / (2 * coef_a)
@@ -108,15 +104,15 @@ def get_erroneous_classification_probabilities(M_l, M_j, B):
     print(f"Ошибка первого рода: {p_0}")
     print(f"Ошибка второго рода: {p_1}")
     # R = 1/2(p_0+p_1) = 1 - Ф(0.5*sqrt(distance))
-    print(f"Суммарный вероятность ошибочной классификации: {p_0+p_1}")
+    print(f"Суммарный вероятность ошибочной классификации: {p_0 + p_1}")
 
 
 def experimental_probability_error(x, M_l, M_j, B_l, B_j, P_l, P_j):
     count = 0
 
     calc_d = lambda vec, M, B, P: (np.log(P) - np.log(np.sqrt(np.linalg.det(B))) -
-                                    0.5 * np.matmul(np.matmul((vec-M), np.linalg.inv(B)), (vec-M).reshape(2, 1))
-                              )  # page 29
+                                   0.5 * np.matmul(np.matmul((vec - M), np.linalg.inv(B)), (vec - M).reshape(2, 1))
+                                   )  # page 29
 
     for xi in x:
         d_l = calc_d(xi, M_l, B_l, 0.5)
@@ -129,11 +125,11 @@ def experimental_probability_error(x, M_l, M_j, B_l, B_j, P_l, P_j):
 
 
 def get_eps(p, N):
-    return np.sqrt((1-p)/(N*p))
+    return np.sqrt((1 - p) / (N * p))
 
 
 def get_N(p, err):
-    return (1-p)/(err**2 * p)
+    return (1 - p) / (err ** 2 * p)
 
 
 def task_1():
@@ -147,14 +143,14 @@ def task_1():
     max_value = max(np.max(sample_1[:, 0]), np.max(sample_2[:, 0]))
 
     x = np.linspace(min_value, max_value, 100)
-    threshold = np.log(0.5/0.5)
+    threshold = np.log(0.5 / 0.5)
     y = boundary_of_bayes_classifier_for_N_with_same_B(x, M_0, M_1, B_0, threshold)
 
-    plt.suptitle("Байесовский классификатор")
-    plt.plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
-    plt.plot(sample_2[:, 0], sample_2[:, 1], color='green', linestyle='none', marker='*')
-    plt.plot(x, y, color="red")
-    plt.show()
+    # plt.suptitle("Байесовский классификатор")
+    # plt.plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
+    # plt.plot(sample_2[:, 0], sample_2[:, 1], color='green', linestyle='none', marker='*')
+    # plt.plot(x, y, color="red")
+    # plt.show()
 
     get_erroneous_classification_probabilities(M_0, M_1, B_0)
 
@@ -180,28 +176,28 @@ def task_2():
     # p_0 = p_1 => Laplace functions equal => lambda = 0 => P(omega0) = P(omega1)
     P_0 = 0.5
     P_1 = 0.5
-    threshold = np.log(P_0/P_1)
+    threshold = np.log(P_0 / P_1)
     y_0 = boundary_of_bayes_classifier_for_N_with_same_B(x, M_0, M_1, B_0, threshold)
 
-    plt.suptitle("Минимаксный классификатор")
-    plt.plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
-    plt.plot(sample_2[:, 0], sample_2[:, 1], color='green', linestyle='none', marker='*')
-    plt.plot(x, y_0, color="red")
-    plt.show()
+    # plt.suptitle("Минимаксный классификатор")
+    # plt.plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
+    # plt.plot(sample_2[:, 0], sample_2[:, 1], color='green', linestyle='none', marker='*')
+    # plt.plot(x, y_0, color="red")
+    # plt.show()
 
     # Neyman-Pearson classifier
     p_0 = 0.05
     distance = Mahalanobis_distance(M_0, M_1, B_0)
     # inv Laplace function for (1-p_0): p_0 = 0.05
     inv_laplace = 1.645
-    lambda_tilda = -0.5*distance + np.sqrt(distance)*inv_laplace
+    lambda_tilda = -0.5 * distance + np.sqrt(distance) * inv_laplace
     y_1 = boundary_of_bayes_classifier_for_N_with_same_B(x, M_0, M_1, B_0, lambda_tilda)
 
-    plt.suptitle("Классификатор Неймана-Пирсона")
-    plt.plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
-    plt.plot(sample_2[:, 0], sample_2[:, 1], color='green', linestyle='none', marker='*')
-    plt.plot(x, y_1, color="red")
-    plt.show()
+    # plt.suptitle("Классификатор Неймана-Пирсона")
+    # plt.plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
+    # plt.plot(sample_2[:, 0], sample_2[:, 1], color='green', linestyle='none', marker='*')
+    # plt.plot(x, y_1, color="red")
+    # plt.show()
 
     print(f"Экспериментальная верояность для Неймана-Пирсона: {get_count_fail(sample_1, M_0, M_1, B_0, lambda_tilda)}")
 
@@ -249,14 +245,35 @@ def task_3():
           f"с погрешностью не более 5%: {n}")
 
 
+def plot_task3(x_1, y_1_1, y_1_2, y_1_3):
+    sample_1 = np.transpose(np.load("Files/arrayX2_1.npy"))
+    sample_2 = np.transpose(np.load("Files/arrayX2_2.npy"))
+    fig, ax = plt.subplots(1, 3)
+    fig.set_size_inches(15, 7)
+    ax[0].set_title("Байесовский классификатор")
+    ax[0].plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
+    ax[0].plot(sample_2[:, 0], sample_2[:, 1], color='green', linestyle='none', marker='*')
+    ax[0].plot(x_1, y_1_1, color="red")
+    ax[1].set_title("Минимаксный классификатор")
+    ax[1].plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
+    ax[1].plot(sample_2[:, 0], sample_2[:, 1], color='green', linestyle='none', marker='*')
+    ax[1].plot(x_1, y_1_2, color="red")
+    ax[2].set_title("Классификатор Неймана-Пирсона")
+    ax[2].plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
+    ax[2].plot(sample_2[:, 0], sample_2[:, 1], color='green', linestyle='none', marker='*')
+    ax[2].plot(x_1, y_1_3, color="red")
+    plt.show()
+
+
 def show_all_borders(borders):
     sample_1 = np.load("Files/arrayX2_1.npy")
     sample_2 = np.load("Files/arrayX2_2.npy")
     sample_1 = np.transpose(sample_1)
     sample_2 = np.transpose(sample_2)
 
-    for border in borders:
-        plt.plot(border[0], border[1], color="red")
+    plt.plot(borders[0][0], borders[0][1], color="red")
+    plt.plot(borders[1][0], borders[1][1], color="purple", linestyle = '-.')
+    plt.plot(borders[2][0], borders[2][1], color="pink")
 
     plt.plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
     plt.plot(sample_2[:, 0], sample_2[:, 1], color='green', linestyle='none', marker='*')
@@ -269,6 +286,7 @@ def main():
     value = task_2()
     x_1, y_1 = value[0]
     x_2, y_2 = value[1]
+    # plot_task3(x_0, y_0, y_1, y_2)
 
     borders = [[x_0, y_0], [x_1, y_1], [x_2, y_2]]
     show_all_borders(borders)
